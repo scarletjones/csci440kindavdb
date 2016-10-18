5e4407f7a311f2656c555536017107b1f1d6faba
scarletjones <scarletjones@gmail.com>
2016-10-17 22:05:23 -0400

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
-   define('DB_SERVER', 'localhost:3036');
+   define('DB_SERVER', 'phptest-146718:us-central1:kvdb1');
    define('DB_USERNAME', 'root');
-   define('DB_PASSWORD', 'rootpassword');
-   define('DB_DATABASE', 'database');
+   define('DB_PASSWORD', 'MtXw7523!');
+   define('DB_DATABASE', 'supercooldb');
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
